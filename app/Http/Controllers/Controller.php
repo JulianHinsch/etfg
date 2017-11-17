7002e620645ddd78cffa544108ddf1dc97c7a8bf
JulianHinsch <JulianHinsch@github.com>
2017-11-17 15:15:29 -0500

    phase 1 complete

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -120,7 +120,7 @@ class ProductsController extends Controller {
     }
     public function getInfoByTicker($ticker) {
         $productInfo = DB::table('products')
-            ->select(DB::raw('products.ticker, products.name, products.region, products.sub_region, products.view_count, products.updated_at, asset_classes.name as asset_class, issuers.issuer_name as issuer, categories.category_name as category'))
+            ->select(DB::raw('products.ticker, products.name, products.development_class, products.region, products.sub_region, products.view_count, products.updated_at, asset_classes.name as asset_class, issuers.issuer_name as issuer, categories.category_name as category'))
             ->where('ticker', $ticker)
             ->join('asset_classes', 'asset_classes.id','=','products.asset_class_id')
             ->join('issuers', 'issuers.issuer_id','=','products.issuer_id')
@@ -164,6 +164,7 @@ class ProductsController extends Controller {
             ->join('firms', 'users.firm_id', '=', 'firms.id')
             ->where('firms.id',$firmId)
             ->orderBy('date','desc')
+            ->limit(20)
             ->get();
         return response()->json($actions);
     }
