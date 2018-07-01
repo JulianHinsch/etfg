1498ccfeae5f93e12d7c317191771b78dc90d89f
JulianHinsch <jhinsch799@gmail.com>
2018-07-01 13:22:51 -0400

    fixed database url password variable

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', $database),
             'username' => env('DB_USERNAME', $username),
-            'password' => env('DB_PASSWORD', $pass),
+            'password' => env('DB_PASSWORD', $password),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
