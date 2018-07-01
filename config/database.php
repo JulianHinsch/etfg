bcce46c463427c93f078dcb212618f808cad9f34
JulianHinsch <jhinsch799@gmail.com>
2018-07-01 12:17:28 -0400

    added procfile

--- config/database.php
+++ config/database.php
@@ -1,14 +1,5 @@
 <?php
 
-
-//these are AWS server environment variables
-
-// define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
-// define('RDS_DB_USERNAME', $_SERVER['RDS_USERNAME']);
-// define('RDS_DB_PASSWORD', $_SERVER['RDS_DB_PASSWORD']);
-// define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
-
-
 return [
 
     'default' => env('DB_CONNECTION', 'mysql'),
@@ -18,14 +9,10 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
-            //'host' => RDS_HOSTNAME,
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'etfg'),
-            //'database' => RDS_DB_NAME,
             'username' => env('DB_USERNAME', 'root@localhost'),
-            //'username' => RDS_DB_USERNAME,
             'password' => env('DB_PASSWORD', '27271301hG'),
-            //'password' => RDS_DB_PASSWORD,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -34,75 +21,6 @@ return [
             'engine' => null,
         ],
 
-         /*
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-        ],*/
-        
-/*
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-        ],
-        */
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
-    |
     
-
-    'migrations' => 'migrations',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer set of commands than a typical key-value systems
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
-    
-
-    'redis' => [
-
-        'client' => 'predis',
-
-        'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
-        ],
-
-    ],
-    */
 ];
