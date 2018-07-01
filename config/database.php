364eaf55bdedd4359ebb873de9737221b9822693
JulianHinsch <jhinsch799@gmail.com>
2018-07-01 12:29:22 -0400

    edited database.php for cleardb

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     'default' => env('DB_CONNECTION', 'mysql'),
@@ -8,11 +15,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'etfg'),
-            'username' => env('DB_USERNAME', 'root@localhost'),
-            'password' => env('DB_PASSWORD', '27271301hG'),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $pass),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
