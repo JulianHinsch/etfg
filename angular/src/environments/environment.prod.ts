3c7686d57ee1ae0bd71967373462ccb3cb7f0d27
JulianHinsch <jhinsch799@gmail.com>
2018-07-29 09:14:03 -0400

    update database.php

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://etfg.herokuapp.com',
-  apiBaseUrl: 'https://etfg.herokuapp.com'
+  apiBaseUrl: 'https://etfg.herokuapp.com/'
 };
--- routes/routes.php
+++ routes/routes.php
@@ -5,21 +5,21 @@ use Illuminate\Http\Request;
 
 Route::group(['middleware' => 'addAuthResponseHeader'], function () {
     // firms
-    Route::get('/firms', array('uses' => 'FirmsController@getAll'));
+    Route::get('/firms', array('uses' => 'FirmsController@getAll')); //broken
     Route::get('/firms/{id}', array('uses' => 'FirmsController@getInfoById'));
     Route::get('/firms/views/{id}', array('uses' => 'FirmsController@getViewsById'));
     Route::get('/firms/actions/{id}', array('uses' => 'FirmsController@getActionsById'));
     // products
-    Route::get('/products', array('uses' => 'ProductsController@getAll'));
+    Route::get('/products', array('uses' => 'ProductsController@getAll')); //broken
     Route::get('/products/{ticker}', array('uses' => 'ProductsController@getInfoByTicker'));
     Route::get('/products/actions/{ticker}', array('uses' => 'ProductsController@getActionsByTicker'));
     Route::get('/products/firms/{ticker}', array('uses' => 'ProductsController@getFirmsByTicker'));
     Route::get('/products/{ticker}/firms/{id}', array('uses' => 'ProductsController@getViewsByFirm'));
     // search
-    Route::get('/search/{term}', array('uses' => 'SearchController@getSearchResults'));
+    Route::get('/search/{term}', array('uses' => 'SearchController@getSearchResults')); //works
     // charts
-    Route::get('/viewsbycountry', array('uses' => 'ChartsController@getViewsByCountry'));
-    Route::get('/viewsbytype', array('uses' => 'ChartsController@getViewsByType'));
+    Route::get('/viewsbycountry', array('uses' => 'ChartsController@getViewsByCountry')); //works
+    Route::get('/viewsbytype', array('uses' => 'ChartsController@getViewsByType')); //works
     Route::get('/viewsbytype/{ticker}', array('uses' => 'ChartsController@getProductViewsByType'));
     Route::get('/viewsbyfirm/{ticker}', array('uses' => 'ChartsController@getProductViewsByFirm'));
 });
